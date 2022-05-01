8b7125e4652c8e4ac45578e4bcb42c802d3cbc12
Quintana Juarez Bryan Alexis <Usuario@DESKTOP-CHAIAKK>
2022-05-01 13:45:12 -0600

    Agregando Número de Control

--- src/main/java/datos/Datos.java
+++ src/main/java/datos/Datos.java
@@ -6,7 +6,7 @@ package datos;
 public class Datos {
 
     public static void main(String[] args) {
-        Informacion info = new Informacion("JGS","Sistemas",3);
+        Informacion info = new Informacion("JGS","Sistemas",21550295);
         //Encapsulamiento
         String NumCon = info.getNombre()+" "+info.getCarrera()+" "+info.getSemestre();
         System.out.println("\nDatos generales alumno(a): "+NumCon);
